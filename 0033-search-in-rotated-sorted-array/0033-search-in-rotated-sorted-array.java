963bf5f204056176ba812e378c62fa08e0d8b38e
saikiran232001 <nagursaikiran01@gmail.com>
2026-06-03 20:01:55 +0530

    Time: 0 ms (100%), Space: 44.1 MB (10.66%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int search(int[] nums, int target) {
+        /*
         int l=0,r=nums.length-1;
         int p=0;
         if(nums[l]<=nums[r]){
@@ -54,6 +55,31 @@ class Solution {
             else{
                 r=mid-1;
             }
+        }
+        return -1;*/
+        int l=0,r=nums.length-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[l]<=nums[mid]){
+                if(nums[mid]>target && nums[l]<=target){
+                    r=mid-1;
+                }
+                else{
+                    l=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && nums[r]>=target){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+            
         }
         return -1;
     }
