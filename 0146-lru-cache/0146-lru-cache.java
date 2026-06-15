748f4a01680f6ee5fd0e71899394b5320fb346df
saikiran232001 <nagursaikiran01@gmail.com>
2026-06-15 14:12:09 +0530

    Time: 51 ms (26%), Space: 131.4 MB (56.58%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,4 +1,17 @@
+class node{
+    int key;
+    int value;
+    node prev;
+    node next;
+    node(int key,int value){
+        this.key=key;
+        this.value=value;
+        prev=null;
+        next=null;
+    }
+}
 class LRUCache {
+    /*
     int capacity;
     int l=0;
     int r=0;
@@ -32,6 +45,112 @@ class LRUCache {
             cache.remove(cache.size()-1);
         }
         cache.add(0,new int[]{key,value});
+    }
+    */
+
+    int capacity;
+    int l=0;
+    node head=null;
+    node tail=null;
+    HashMap<Integer,node> m1;
+    public LRUCache(int capacity) {
+        this.capacity=capacity;
+        m1=new HashMap<>();
+    }
+    
+    public int get(int key) {
+        if(m1.containsKey(key)){
+            node temp=m1.get(key);
+            if(temp.prev==null){
+                head=temp;
+                return temp.value;
+            }
+            if(temp.next!=null && temp.prev!=null){
+                temp.prev.next=temp.next;
+                temp.next.prev=temp.prev;
+
+                temp.next=head;
+                head.prev=temp;
+                head=temp;
+                head.prev=null;
+            }
+            else{
+                if(temp.next==null){
+                    tail=temp.prev;
+                    temp.next=head;
+                    temp.prev.next=null;
+                    temp.prev=null;
+                    head.prev=temp;
+                    head=temp;
+                }
+            }
+            return m1.get(key).value;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if(m1.containsKey(key)){
+            node temp=m1.get(key);
+            temp.value=value;
+            if(temp.prev==null){
+                return;
+            }
+            if(temp.next!=null && temp.prev!=null){
+                temp.prev.next=temp.next;
+                temp.next.prev=temp.prev;
+
+                temp.next=head;
+                head.prev=temp;
+                temp.prev=null;
+                head=temp;
+            }
+            else{
+                if(temp.next==null){
+                    tail=temp.prev;
+                    temp.next=head;
+                    temp.prev.next=null;
+                    temp.prev=null;
+                    head.prev=temp;
+                    head=temp;
+                }
+            }     
+        }
+        else{
+            if(l<capacity){
+                l++;
+                node n1=new node(key,value);
+                m1.put(key,n1);
+                if(head==null){
+                    head=n1;
+                    tail=n1;
+                }
+                else{
+                    n1.next=head;
+                    head.prev=n1;
+                    head=n1;
+                }
+            }
+            else{
+                node n1=new node(key,value);
+                m1.remove(tail.key);
+                if(capacity==1){
+                    head=n1;
+                    tail=n1;
+                    m1.put(key,n1);
+                    return;
+                }
+                if(capacity>1){
+                    tail=tail.prev;
+                    tail.next=null;
+                }
+                m1.put(key,n1);
+                n1.next=head;
+                head.prev=n1;
+                head=n1;
+                
+            }
+        }
     }
 }
 
