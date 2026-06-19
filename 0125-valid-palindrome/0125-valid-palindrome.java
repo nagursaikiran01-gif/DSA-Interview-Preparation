36908970ae9dd269a1c1f9522c388c55ddfe28e2
saikiran232001 <nagursaikiran01@gmail.com>
2026-06-19 15:56:01 +0530

    Time: 128 ms (5.47%), Space: 48.1 MB (6.24%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String res="";
+        for(char c:s.toCharArray()){
+            int r=c;
+            if((r>=65 && r<=90) || (r>=97 && r<=122) || (r>=48 && r<=57)){
+                res+=Character.toLowerCase(c);
+            }
+        }
+        int n=res.length();
+        for(int i=0;i<n/2;i++){
+            if(res.charAt(i)!=res.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
