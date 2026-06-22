274f59af507b489f0c4b83733b000adc04216802
saikiran232001 <nagursaikiran01@gmail.com>
2026-06-22 08:16:42 +0530

    Time: 0 ms (100%), Space: 42.8 MB (50.96%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -21,9 +21,12 @@ class Solution {
         if(p==null || q==null){
             return false;
         }
-        if(p.val==q.val){
-            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        if(p.val!=q.val){
+            return false;
         }
-        return false;
+        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
+
+//O(n)-time
+//O(n)-space
