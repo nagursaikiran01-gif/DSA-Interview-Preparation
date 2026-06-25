0430bc9dfcebc5a093af627718430ceaee48dd1a
saikiran232001 <nagursaikiran01@gmail.com>
2026-06-25 18:22:08 +0530

    Time: 0 ms (100%), Space: 47.2 MB (81.35%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int total=0;
+        for(int i=0;i<n;i++){
+            total+=nums[i];
+        }
+        int k=n*(n+1)/2;
+        return k-total;
+    }
+}
